b070406a0cc06ce54f57096398ef5ef6344dd2f8
Oqkring <serpegcg@gmail.com>
2023-03-17 23:22:24 +0100

    // Perfezionato

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+T
+$GeneratoreCasualePasswordConsole.iml,a\c\acced6cb5583d38221abdf20428dfd3f4718747f
--- src/generatore/Main.java
+++ src/generatore/Main.java
@@ -2,49 +2,58 @@ package generatore;
 
 
 import java.security.SecureRandom;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
-public class generaPassword {
+public class Main {
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
+    static final char[] alfabeto = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z'};
+    static final char[] simboli = new char[]{')', '~', '`', '!', '@', '#', '$', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"', '<', ',', '>', '.', '?'};
 
-
-    static final char[] alfabeto = new char[]{
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z'
-    };
-    static final char[] simboli = new char[]{
-            ')', '~', '`', '!', '@', '#', '$', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '[', '}', ']', '|', ':', ';', '"', '<', ',', '>', '.', '?'
-    };
 
     public static void main(String[] args) {
-    	int nPassword = inputIntero(ANSI_GREEN + "Digita quantità password richieste" + ANSI_RESET);
-        caratteristichePassword(inputIntero(ANSI_GREEN + "Digita lunghezza password"), nPassword);
+
+        int nPassword = inputInt("\nDigita quantità password richieste");
+
+        int percentualeLettere = restringiTra(inputInt("\nDigita, in percentuale, la possibilità  di uscita di lettere  (0-100)"), 0, 100);
+        int percentualeSimboli = restringiTra(inputInt("Digita, in percentuale, la possibilità di uscita di simboli (0-" + (100-percentualeLettere) + ")"),0,100-percentualeLettere);
+        System.out.println("Rimanente possibilità uscita numeri = " + (100-(percentualeLettere+percentualeSimboli)));
+
+        int lunghezzaPassword = inputInt("\nDigita lunghezza passwords");
+
+        System.out.println("\nLista Password :\n");
+
+        String[] passwords = new String[nPassword];
+        for (int i = 0; i < nPassword; i++) {
+            passwords[i] = generaPassword(percentualeLettere, percentualeSimboli, lunghezzaPassword);
+        }
+        for(String output : controlloDuplicati(passwords, percentualeLettere, percentualeSimboli))
+            System.out.println(output);
+
+        if(inputInt("\nDigita 0 per interrompere") != 0)
+            main(null);
     }
 
-    private static void caratteristichePassword(int lunghezzaPassword, int nPassword) {
-        System.out.println(ANSI_GREEN + "Digita prima la percentuale delle lettere poi la percentuale dei simboli e il rimarente sarà la percentuale dei numeri.");
-        int percentualeLettere = restringiTra(inputIntero(ANSI_GREEN + "Digita percentuale possibilità uscita lettere  (0-100)"), 0, 100);
-        int percentualeSimoboli = restringiTra(inputIntero(ANSI_GREEN + "Digita percentuale possibilità uscita simboli (0-" + (100-percentualeLettere) + ")"),percentualeLettere,100);
-        System.out.println(ANSI_GREEN + "Rimanente possibilità uscita numeri = " + (100-(percentualeLettere+percentualeSimoboli)));
-        System.out.println(ANSI_YELLOW + "Password : ");
-        for (int i = 0; i < nPassword; i++) {
-            generaPassword(percentualeLettere, percentualeSimoboli, lunghezzaPassword);
+    private static ArrayList<String> controlloDuplicati(String[] arrayDaControllare, int percentualeLettere, int percentualeSimboli) {
+
+        ArrayList<String> passwordsDistinte = new ArrayList<>(Arrays.stream(arrayDaControllare).distinct().toList());
+
+        if (arrayDaControllare.length != passwordsDistinte.size()) {
+            for (int i = 0; i < arrayDaControllare.length - passwordsDistinte.size(); i++) {
+                passwordsDistinte.add(generaPassword(percentualeLettere, percentualeSimboli, arrayDaControllare.length));
+            }
+            controlloDuplicati(passwordsDistinte.toArray(new String[0]), percentualeLettere, percentualeSimboli);
         }
+        return passwordsDistinte;
     }
-    
-    private static void generaPassword(int percentualeLettere, int percentualeSimoboli, int lunghezzaPassword) {
+
+    private static String generaPassword(int percentualeLettere, int percentualeSimboli, int lunghezzaPassword) {
         StringBuilder passwordBuilder = new StringBuilder();
         for (int i = 0; i < lunghezzaPassword; i++)
-            carattereCasuale(passwordBuilder, percentualeLettere, percentualeLettere + percentualeSimoboli);
+            carattereCasuale(passwordBuilder, percentualeLettere, percentualeLettere + percentualeSimboli);
 
-        System.out.println(ANSI_PURPLE + passwordBuilder + ANSI_RESET);
+        return passwordBuilder.toString();
     }
-    
+
     private static void carattereCasuale(StringBuilder passwordBuilder, int percentualeLettere, int percentualeSimboli) {
         int percentuale = new SecureRandom().nextInt(101);
 
@@ -57,26 +66,23 @@ public class generaPassword {
 
     }
 
-    private static int inputIntero(String prefisso) {
+    public static int restringiTra(int value, int min, int max) {
+        return Math.min(max,Math.max(value, min));
+    }
 
-        System.out.print((!prefisso.equals("") ? prefisso + ": " : "") + ANSI_RESET);
+    private static int inputInt(String prefisso) {
+
+        System.out.print((!prefisso.equals("") ? prefisso + ": " : ""));
 
         int output;
 
-        Scanner input = new Scanner(System.in);
-
         try {
-            output = input.nextInt();
+            output = new Scanner(System.in).nextInt();
         } catch (InputMismatchException e) {
-            output = inputIntero(ANSI_RED + "Tipo input errato riprovare" );
+            output = inputInt("Tipo input errato riprovare" );
         }
-        input.close();
-        return output;
-    }
 
-    public static int restringiTra(int value, int min, int max)
-    {
-        return (value < min) ? min : (value > max) ? max : value ;
+        return output;
     }
 
 }
